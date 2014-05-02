e995bb252e23251f2283199613e2611a37371e45
Makis Otman <makisotman@gmail.com>
2014-05-02 09:15:45 +0100

    methods for greeting the player and getting the name

--- lib/game_sequence.rb
+++ lib/game_sequence.rb
@@ -1,9 +1,26 @@
 class GameSequence
-  attr_reader :interface, :ai
+  attr_reader :interface, :ai, :output, :input
 
-  def initialize(interface, ai)
+  def initialize(interface, ai, output = nil, input = nil)
     @interface = interface
     @ai = ai
+    @output = $stdout
+    @input = $stdin
   end
 
+  def greet_player
+    output.puts 'Welcome to Tic Tac Toe.'
+  end
+
+  def get_name
+    output.puts 'What shall I call you today?'
+    name = get_input
+    name
+  end
+
+  private
+
+  def get_input
+    input.gets.chomp.capitalize
+  end
 end
--- spec/game_sequence_spec.rb
+++ spec/game_sequence_spec.rb
@@ -14,4 +14,19 @@ describe GameSequence do
     expect(game.ai).to eq computer
   end
 
+  context '#greet_player' do
+    it 'prints a greeting message' do
+      game.output.stub(:puts) { 'Welcome to Tic Tac Toe.' }
+      game.greet_player
+    end
+  end
+
+  context '#get_name' do
+    it 'asks the user for the name' do
+      game.output.stub(:puts) { 'What shall I call you today?' }
+      game.input.stub(:gets) { 'joe' }
+      expect(game.get_name).to eq 'Joe'
+    end
+  end
+
 end
