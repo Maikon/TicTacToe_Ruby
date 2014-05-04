27553901b0d5cc5376d55557fffe79aade0d5e60
Makis Otman <makisotman@gmail.com>
2014-05-04 20:17:16 +0100

    moving greeting method from sequence to interface

--- lib/game_interface.rb
+++ lib/game_interface.rb
@@ -1,8 +1,18 @@
 class GameInterface
-  attr_reader :board, :player
+  attr_reader :board, :player, :output, :input
 
   def initialize(board, player)
     @board = board
     @player = player
+    @output = $stdout
+    @input = $stdin
+  end
+
+  def greet_player
+    output.puts '-----------------------'
+    output.puts 'Welcome to Tic Tac Toe!'
+    output.puts '-----------------------'
+  end
+
   end
 end
--- spec/game_interface_spec.rb
+++ spec/game_interface_spec.rb
@@ -1,9 +1,10 @@
+require 'stringio'
 require 'game_interface'
 
 describe GameInterface do
   let(:board_mock)    { double :board }
-  let(:opponent) { double :human }
-  let(:interface) { GameInterface.new(board_mock, opponent) }
+  let(:opponent)      { double :human }
+  let(:interface)     { GameInterface.new(board_mock, opponent) }
 
   context '#initialized' do
     it 'with a board' do
@@ -12,6 +13,15 @@ describe GameInterface do
 
     it 'with a player' do
       expect(interface.player).to eq opponent
+    end
+  end
+
+  context '#greet_player' do
+    it 'prints a greeting message' do
+      expect(interface.output).to receive(:puts).with('-----------------------')
+      expect(interface.output).to receive(:puts).with('Welcome to Tic Tac Toe!')
+      expect(interface.output).to receive(:puts).with('-----------------------')
+      interface.greet_player
     end
   end
 end
